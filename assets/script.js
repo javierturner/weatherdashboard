9451ec99ad904600d1e2127abd5887edc270d244
JT Turner <j.tturner14@yahoo.com>
2019-11-22 17:41:17 -0500

    created basic layout html/css

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+//TO DO////////////////////////////////////////////////////////////
+    //build weather dashboard with search functionality to find current    weather conditions and future weather outlook for multiple cities
+    //OpenWeather API (How To Start in documentation https://openweathermap.org/api)
+        //Use AJAX to hook into the API and retrieve data in JSON format
+        //JQUERY!!!
+        
+
+//DISPLAY REQUIREMENTS///////////////////////////////////////////////
+    //Current Weather Conditions
+        //City
+        //Date
+        //Icon Image (visual representation of weather conditions)
+        //Temperature
+        //Humidity
+        //Wind Speed
+        //UV Index
+    //5 Day Forecast
+        //Date
+        //Icon Image
+        //Temp
+        //Humidity
+    //Search History (users can access past search terms. 
+        //Clicking on the city name will perform an updated search with     current and future conditions)
+    
+
+//HINTS////////////////////////////////////////////////////////////
+    //Create multiple functions within app to handle diff parts of the dashboard
+        //Current conditions
+        //5-day forecast
+        //Search history
+        //UV Index
+    //Make more than one AJAX call
+    //Hardcode some parameters in the API's URL
+        //User input will determine some of the other parameters
+    //Use LOCALSTORAGE 
+
+    
+///BONUS///////////////////////////////////////////////////////////
+    //Use Geolocation API to add user's current location to the initial landing page
+    //Add app to portfolio
