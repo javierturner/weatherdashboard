daeeb7e071eae4662240da9dbce8d1c423e750ab
JT Turner <j.tturner14@yahoo.com>
2019-11-27 22:45:37 -0500

    show/hide weather info and started css

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,37 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",USA&units=imperial&appid=166a433c57516f51dfab1f7edaed8413";
-    $.ajax({
-      url: queryURL,
-      method: "get"
-    }).then(function (response) {
-      console.log(response)
+var hideInfo = document.getElementById("currentCity");
+hideInfo.style.display = "none";
 
+function showWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",USA&units=imperial&appid=1f890b05c24ad48ca60b1cd075d3c1e7";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+
+        var date = new Date();
+        date = moment().format('LL');
+        var cityInfo = $("#cityInfo");
+        var cityName = $("<h1>").text(response.name);
+        var URL = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        var showIcon = $("<img>").attr("src", URL);
+        var temperature = $("<p>").text("Temperature: " + response.main.temp + " °F");
+        var humidityElement = $("<p>").text("Humidity: " + response.main.humidity + "%");
+        var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
+
+        hideInfo.style.display = "block";
+
+        cityName.append(" (" + date + ")");
+        cityName.append(showIcon);
+        cityInfo.append(cityName, temperature, humidityElement, windSpeed);
+        $("#cityInfo").prepend(cityInfo);
+        //   showInfo.style.display = "block";
     });
+}
+
+$("#submitButton").on("click", function (event) {
+    event.preventDefault();
+    var inputWeather = $("#citySearched").val().trim();
+    showWeather(inputWeather);
+});
